2854fad6e52be75506a5ee502d9b8fc91d14bde8
joe <you@example.com>
2021-06-19 15:24:16 -0400

    Fix navigation.

--- site/selection.js
+++ site/selection.js
@@ -1,39 +1,11 @@
 function getAbout(series)
 {
-    getEpisode(series, -1);
-//     var result  = "All about it."
-//     var xhttp   = new XMLHttpRequest();
-//     xhttp.onreadystatechange = function()
-//     {
-//          alert(this.status)
-//         if (this.readyState == 4 && this.status == 200)
-//         {
-//         document.getElementById("view").innerHTML = this.responseText;
-//         }
-//     };
-//     xhttp.open("GET", "about.txt", true);
-//     xhttp.send();
-//     return(result);
+    var file    = "about.txt";
+    getEpisode(file);
 }
 
-// constructor(series)
-// {
-//     document.write("\nHello");
-//     this.series     = series;
-//     this.episodes   = [1,2,3];
-//     this.i          = this.episodes.length - 1;
-// }
-
-// getSeries()
-// {
-//     var result          = this.series;
-//     return(result);
-// //  document.write(this.series);
-// }
-
-function getEpisode(series, i)
+function getEpisode(file)
 {
-    i++;
     var xhttp   = new XMLHttpRequest();
     xhttp.onreadystatechange = function()
     {
@@ -43,10 +15,8 @@ function getEpisode(series, i)
         document.getElementById("view").innerHTML = this.responseText;
         }
     };
-//     alert(series + "_" + i.toString() + ".txt");
-    var episode     = series + "_" + i.toString() + ".txt"
-    alert(episode)
-    xhttp.open("GET", episode, true);
+//     alert(file)
+    xhttp.open("GET", file, true);
     xhttp.send();
 //     return(i);
 }
@@ -54,24 +24,41 @@ function getEpisode(series, i)
 function first(series, i)
 {
     i               = 0;
-    getEpisode(series, i);
+    num             = i + 1;
+    var file        = series[0] + "_" + num.toString() + ".txt"
+    getEpisode(file);
     return(i);
 }
 
 function previous(series, i)
 {
-    i --;
+    if(i > 0)
+    {
+        i --;
+        num         = i + 1;
+        var file    = series[0] + "_" + num.toString() + ".txt"
+        getEpisode(file);
+    }
     return(i);
 }
 
 function next(series, i)
 {
-    i ++;
+    if(i < series.length - 2)
+    {
+        i ++;
+        num         = i + 1;
+        var file    = series[0] + "_" + num.toString() + ".txt"
+        getEpisode(file);
+    }
     return(i);
 }
 
 function last(series, i)
 {
-    i           = episodes.length - 1;
+    i               = series.length - 2;
+    num             = i + 1;
+    var file        = series[0] + "_" + num.toString() + ".txt"
+    getEpisode(file);
     return(i);
 }
