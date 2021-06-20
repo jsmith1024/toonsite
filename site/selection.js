beec7ce47ff94d29a0c90faa0ba33706212be88b
joe <you@example.com>
2021-06-20 09:21:23 -0400

    Convert to data structure.

--- site/selection.js
+++ site/selection.js
@@ -1,3 +1,27 @@
+function getSeries(name)
+{
+//     var xhttp   = new XMLHttpRequest();
+//     xhttp.onreadystatechange = function()
+//     {
+//         alert(this.status)
+//         if (this.readyState == 4 && this.status == 200)
+//         {
+// //         document.getElementById("view").innerHTML = this.responseText + '</p><img src="' + file + '.jpg"></img>';
+// //         import series from name + '.json';
+//             var data    = callback(this.responseText);
+//             alert("data: " + data);
+//             var series  = parse(data);
+//         }
+//     };
+//     alert(file);
+//     xhttp.open("GET", name + ".json", true);
+//     xhttp.send();
+//     alert("series: " + series["name"]);
+//     document.write("</p>TYPE:&nbsp;&nbsp;" + typeof(series));
+//     return(series);
+return({"title": "TEST", "about": "This is a test, only a test.", "Episodes": [{ "number": 001, "story": "the test", "part": 1, "notes": "hello"}, { "number": 002, "story": "the test", "part": 2, "notes": "hello"}, { "number": 003, "story": "the test", "part": 3, "notes": "hello"}] });
+}
+
 function getEpisode(file)
 {
     var xhttp   = new XMLHttpRequest();
@@ -9,60 +33,59 @@ function getEpisode(file)
         document.getElementById("view").innerHTML = this.responseText + '</p><img src="' + file + '.jpg"></img>';
         }
     };
-    alert(file);
+//     alert(file);
     xhttp.open("GET", file + ".txt", true);
     xhttp.send();
 //     return(i);
 }
 
-function getAbout(name, series)
+function getAbout(series)
 {
-    var file    = "about";
-    getEpisode(file);
+    document.getElementById("view").innerHTML = series["about"];
 }
 
-function first(name, series, i)
+function first(series, i)
 {
-    alert("name: " + name);
     i               = 0;
     num             = i + 1;
-    var file        = name + "_" + num.toString()
+//     alert("name: " + series["title"] + num);
+    var file        = series["title"] + "_" + num.toString();
     getEpisode(file);
     return(i);
 }
 
-function previous(name, series, i)
+function previous(series, i)
 {
     if(i > 0)
     {
-    alert("name: " + name);
         i --;
         num         = i + 1;
-        var file    = name + "_" + num.toString()
+//         alert("name: " + series["title"] + num);
+        var file    = series["title"] + "_" + num.toString();
         getEpisode(file);
     }
     return(i);
 }
 
-function next(name, series, i)
+function next(series, i)
 {
-    if(i < series.length - 2)
+    if(i < series["Episodes"].length - 1)
     {
-    alert("name: " + name);
         i ++;
         num         = i + 1;
-        var file    = name + "_" + num.toString()
+//         alert("name: " + series["title"] + num);
+        var file    = series["title"] + "_" + num.toString();
         getEpisode(file);
     }
     return(i);
 }
 
-function last(name, series, i)
+function last(series, i)
 {
-    alert("name: " + name);
-    i               = series.length - 2;
+    i               = series["Episodes"].length - 1;
     num             = i + 1;
-    var file        = name + "_" + num.toString()
+//     alert("name: " + series["title"] + num);
+    var file        = series["title"] + "_" + num.toString();
     getEpisode(file);
     return(i);
 }
