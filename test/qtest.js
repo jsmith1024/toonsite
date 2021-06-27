dca03ddbaf4fb0cdf269612a123d65608d3c4a50
joe <you@example.com>
2021-06-26 21:35:13 -0400

    Test, in qunit, if seres not null.

--- test/qtest.js
+++ test/qtest.js
@@ -2,33 +2,22 @@ QUnit.module('control');
 
 QUnit.test( "Test", async assert =>
 {
-    getData();
+//     getData();
     var name    = "TEST";
     var series  = null;
     var length  = 0;
     var index   = 3;
-    update();   // to a consistent state
+//     update();   // to a consistent state
     getData();  // to use, which will update() when ready
-//     let i       = 0;
-//     while(i < 10000000)
-//     {
-//         i      += 1;
-//     }
     assert.equal( index, 3 );
+    let i       = 0;
+    while(i < 1000000000)
+    {
+        i      += 1;
+    }
+    assert.notEqual( null, series );
     updateToFirst();
-//     i           = 0;
-//     while(i < 10000000)
-//     {
-//         i      += 1;
-//     }
-    assert.equal( iindex, 1 );
-//     QUnit.module('control', assert.equal( i, 3 ))
-//     QUnit.test('should add two numbers', function(assert) {
-//       assert.equal(add(1, 1), 2);
-//     });
-//     QUnit.module('add', function() {
-//     QUnit.test('should add two numbers', function(assert) {
-//       assert.equal(add(1, 1), 2);
+    assert.equal( index, 1 );
 //     });
 });
 
