c33d3036d19f033c509c587acc8e8bd46c10d3a1
joe <you@example.com>
2021-06-30 08:02:47 -0400

    Replace toon.js with episode.js, replacing series with length.

--- test/qtest.js
+++ test/qtest.js
@@ -2,36 +2,15 @@ QUnit.module('control');
 
 QUnit.test( "Test", async assert =>
 {
-//     getData();
-//     var name    = "TEST";
-    var series  = null;
-//     getData();
-//     var length  = 0;
-//     var index   = 0;
-//     update();   // to a consistent state
-//     assert.equal( "TEST",   name );
-//     assert.equal( null,     series );
-//     getData();  // to use, which will update() when ready
-//     assert.equal( index,    0 );
-//     let i       = 0;
-//     while(i < 1000000000)
-//     {
-//         i      += 1;
-//     }
-//     assert.notEqual( null,  series );
-//     updateToLast();
-//     assert.equal( index,    3 );
-//     });
-    
-    assert.equal( 0,        toon.index );
-    toon.last();
-    assert.equal( 3,        toon.index );
-    toon.first();
-    assert.equal( 1,        toon.index );
-    toon.next();
-    assert.equal( 2,        toon.index );
-    toon.previous();
-    assert.equal( 1,        toon.index );
+    assert.equal( 0,        episode.index );
+    episode.last();
+    assert.equal( 3,        episode.index );
+    episode.first();
+    assert.equal( 1,        episode.index );
+    episode.next();
+    assert.equal( 2,        episode.index );
+    episode.previous();
+    assert.equal( 1,        episode.index );
     
 });
 
