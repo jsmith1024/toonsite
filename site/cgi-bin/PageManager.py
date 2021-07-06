1619f8acfc7836cb5057c8c691d706600ec1445b
joe <you@example.com>
2021-07-06 13:26:52 -0400

    Cleanup and get 'about' working.

--- site/cgi-bin/PageManager.py
+++ site/cgi-bin/PageManager.py
@@ -3,6 +3,10 @@
 #   @authors joe smith
 
 import fileinput
+import json
+import cgi
+import cgitb
+cgitb.enable()
 
 ##  @class  PageManager
 #   @brief  Manage a simple CGI page.
@@ -17,24 +21,60 @@ class PageManager():
         #self.__Model        = Model
         #self.__View         = View
         #self.__Controller   = Controller
-        filename            = "index.txt"
+        form                = cgi.FieldStorage()
+        if form.getfirst('title'):
+            self.__title    = form.getfirst('title')
+        else:
+            self.__title    = "TEST"
+        if form.getfirst('index'):
+            self.__index    = int(form.getfirst('index'))
+        else:
+            self.__index    = 0
+        if form.getfirst('action'):
+            self.__action   = form.getfirst('action')
+        else:
+            self.__action   = 'last'
+        filename            = "cgi-bin/index.txt"
         with fileinput.input(files=(filename)) as f:
             for line in f:
                 print(line)
+        with open(self.__title + '/'+ self.__title + '.json', 'r') as myfile:
+            data=myfile.read()
+        self.__series       = json.loads(data)
+        print("<script>document.getElementById(\"title_bar\").innerHTML = \"" + self.__series["meta"]["title"] + "\";</script>")
+        print("<script>document.getElementById(\"title\").innerHTML     = \"" + self.__series["meta"]["title"] + "\";</script>")
     
     ##  run
     #   @brief  Page run loo, which handles events.
     def run(self):
-        #while(true):
-            ## catch event
-            # seperate theread(s) ???
-            # if detect action: ???
-            #action  = ???
-            #output  = self.__Model.setAction(action)
-            #content = self.__View.setOut(output)
-            #self.__setDiv("data", content)
-            #self.__update()
-            #self.__setDiv()
+        print("<script>document.getElementById(\"about\").disabled      = false;</script>")
+        print("<script>document.getElementById(\"title\").innerHTML     = series[\"meta\"][\"title\"];</script>")
+        print("<script>document.getElementById(\"first\").disabled      = false;</script>")
+        print("<script>document.getElementById(\"previous\").disabled   = (episode.index <= 1);</script>")
+        print("<script>document.getElementById(\"reload\").disabled     = false;</script>")
+        print("<script>document.getElementById(\"next\").disabled       = episode.index >= (length - 1);</script>")
+        print("<script>document.getElementById(\"last\").disabled       = false;</script>")
+        
+        episode = self.__series["episodes"][1]
+        data    = "Episode #" + episode["number"]
+        data   += "<h1>" + episode["story"] + " - Part" + episode["part"] + "</h1>"
+        data   += "<img src=\"" + self.__title + "/episodes/" + self.__title + "_" + episode["number"] + ".jpg\">"
+        data   += "<p>" + episode["notes"] + "</p>"
+        print(data)
+        #self.__setDiv("data", data)
+        
+        length              = len(self.__series["episodes"])
+        if  (self.__action == "about") or (self.__action == "reload"):
+            self.__setDiv("data", data)
+        elif self.__action == "first":
+            self.__index    = 0
+            self.__setDiv("data", data)
+        # Add previous and next.
+        elif self.__action == "last":
+            self.__index    = length - 1
+            self.__setDiv("data", data)
+        else:
+            raise ValueError("Invalid request.")
         pass
     
     ##  update
@@ -43,7 +83,7 @@ class PageManager():
         pass
 
     def __setDiv(self, div, data):
-        print("here")
+        #print("here")
         script  = "<script>document.getElementById(\"" + div + "\").innerHTML = \"" + data + "\";</script>"
         print(script)
 
--- site/cgi-bin/index.py
+++ site/cgi-bin/index.py
@@ -4,68 +4,66 @@ import cgi
 import cgitb
 cgitb.enable()
 
-def setDiv(div, data):
-    #print("here")
-    script  = "<script>document.getElementById(\"" + div + "\").innerHTML = \"" + data + "\";</script>"
-    print(script)
+#def setDiv(div, data):
+    ##print("here")
+    #script  = "<script>document.getElementById(\"" + div + "\").innerHTML = \"" + data + "\";</script>"
+    #print(script)
 
 #def doThis(resource):
     #print("here: " + resource)
     #script  = "http://localhost:8000/cgi-bin/index.py&" + resource
     #print(script)
 
-print("Content-Type: text/html\n\n")
-print("<html>")
-print("\t<!DOCTYPE html>")
-print("\t<head>")
-print('\t\t<meta http-equiv="Content-Type" content="text/html; charset=utf-8">')
-print("\t\t<title>TOONS!</title>")
-print("\t\t<Content-Type: text/html>")
-print("\t</head>")
-print("\t<body>")
-print("""
-        <div id="controls">
-            <h1 id="title">Coming Soon...</h1>
-            <a href="http://localhost:8000/cgi-bin/index.py?action=about"><button>About</button></a>
-            </br>
-            <button type="button" class="nav" id="first"    disabled>First</button>
-            <button type="button" class="nav" id="previous" disabled>Prev</button>
-            <button type="button" class="nav" id="reload"   onclick="reload()"              disabled>Ret</button>
-            <button type="button" class="nav" id="next"     disabled>Next</button>
-            <button type="button" class="nav" id="last"     disabled>Last</button>
-        </div>		
-        <hr>
-        <div class="data" id="meta">
-            Initial meta.
-        </div>
-        <div class="data" id="view">
-            Initial view.
-        </div>
-    """)
-form = cgi.FieldStorage()
-print("form:")
-print(form)
-user = form.getfirst("user", "").upper()    # This way it's safe.
-for item in form.getlist("item"):
-    print(item)
-print("keys:")
-print(form.keys())
-print("EOF")
-print(form.getfirst('action'))
-print('<script>document.getElementById("view").innerHTML = "hello";</script>')
-setDiv("view", "HELLO")
-print("\t</body>")
-print("</html>")
-setDiv("view", "YAY!!")
+#print("Content-Type: text/html\n\n")
+#print("<html>")
+#print("\t<!DOCTYPE html>")
+#print("\t<head>")
+#print('\t\t<meta http-equiv="Content-Type" content="text/html; charset=utf-8">')
+#print("\t\t<title>TOONS!</title>")
+#print("\t\t<Content-Type: text/html>")
+#print("\t</head>")
+#print("\t<body>")
+#print("""
+        #<div id="controls">
+            #<h1 id="title">Coming Soon...</h1>
+            #<a href="http://localhost:8000/cgi-bin/index.py?index=0&action=about"><button disabled>About</button></a>
+            #</br>
+            #<button type="button" class="nav" id="first"    disabled>First</button>
+            #<button type="button" class="nav" id="previous" disabled>Prev</button>
+            #<button type="button" class="nav" id="reload"   onclick="reload()"              disabled>Ret</button>
+            #<button type="button" class="nav" id="next"     disabled>Next</button>
+            #<button type="button" class="nav" id="last"     disabled>Last</button>
+        #</div>		
+        #<hr>
+        #<div class="data" id="meta">
+            #Initial meta.
+        #</div>
+        #<div class="data" id="view">
+            #Initial view.
+        #</div>
+    #""")
+#form = cgi.FieldStorage()
+#print("form:")
+#print(form)
+#user = form.getfirst("user", "").upper()    # This way it's safe.
+#for item in form.getlist("item"):
+    #print(item)
+#print("keys:")
+#print(form.keys())
+#print("EOF")
+#print(form.getfirst('action'))
+#print(int(form.getfirst('index')))
+#print('<script>document.getElementById("view").innerHTML = "hello";</script>')
+#setDiv("view", "HELLO")
+#print("\t</body>")
+#print("</html>")
+#setDiv("view", "YAY!!")
 
 ###############################################
 ## Using PageManager class to replace above: ##
 ###############################################
 
-#from ToonModel      import ToonModelv
-#from ToonView       import ToonView
-#from ToonController import ToonController
-#from PageManager    import PageManager
+from PageManager    import PageManager
 
-#Page    = PageManager(ToonModel(), ToonView(), ToonController())
-#Page.run()
+Page    = PageManager()
+Page.run()
