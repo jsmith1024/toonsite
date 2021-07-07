2959c2bf3bda79b3086c7d5f965af35821cd935a
joe <you@example.com>
2021-07-06 22:32:34 -0400

    Get page working.

--- site/cgi-bin/PageManager.py
+++ site/cgi-bin/PageManager.py
@@ -39,54 +39,68 @@ class PageManager():
         if not form.getfirst('index'):
             self.__index    = len(self.__series["episodes"]) - 1
         
-        base    = '<a href="http://localhost:8000/cgi-bin/index.py?title=' + self.__title + '&index=' + str(self.__index) + '&action='
-        #data    = '<h1 id="title">Coming Soon...</h1>\n'
-        data    = base + 'about><button     id="about"      disabled>About</button> </a>\n'
-        data   += "</br>\n"
-        data   += base + 'first><button     id="first"      disabled>First</button> </a>\n'
-        data   += base + 'previous><button  id="previous"   disabled>Prev</button>  </a>\n'
-        data   += base + 'reload><button    id="reload"     disabled>Ret</button>   </a>\n'
-        data   += base + 'next><button      id="next"       disabled>Next</button>  </a>\n'
-        data   += base + 'last><button      id="last"       disabled>Last</button>  </a>\n'
-        #print(data)
-        print('<script>document.getElementById("controls").innerHTML = "' + data + '";</script>')
+        base        = '<a href="http://localhost:8000/cgi-bin/index.py?title=' + self.__title + '&index='
+        index       = str(self.__index)
+        data        = base + index + '&action=about\">      <button id="about"      disabled>About</button> </a>\n'
+        data       += "</br>\n"
+        index       = str(1)
+        data       += base + index +  '&action=first\">      <button id="first"      disabled>First</button> </a>\n'
+        index       = str(self.__index - 1)
+        data       += base + index +  '&action=previous\">   <button id="previous"   disabled>Prev</button>  </a>\n'
+        index       = str(self.__index)
+        data       += base + index +  '&action=reload\">     <button id="reload"     disabled>Ret</button>   </a>\n'
+        index       = str(self.__index + 1)
+        data       += base + index +  '&action=next\">       <button id="next"       disabled>Next</button>  </a>\n'
+        index       = str(len(self.__series["episodes"]) - 1)
+        data       += base + index +  '&action=last\">       <button id="last"       disabled>Last</button>  </a>\n'
+        print(data)
+        #print("<script>document.getElementById(\"controls\").innerHTML  = \"" + data + "\";</script>")
+        print("<hr>")
+        print("<script>document.getElementById(\"title\").innerHTML     = \"<h1>" + self.__series["meta"]["title"] + "</h1>\";</script>")
     
     ##  run
     #   @brief  Page run loo, which handles events.
     def run(self):
         print('<script>document.getElementById("about").disabled      = false;</script>')
-        print('<script>document.getElementById("title").innerHTML     = series[\"meta\"][\"title\"];</script>')
+        print('<script>document.getElementById("title").innerHTML     = ' + self.__series["meta"]["title"] + ';</script>')
         print('<script>document.getElementById("first").disabled      = false;</script>')
-        print('<script>document.getElementById("previous").disabled   = (episode.index <= 1);</script>')
+        if self.__index <= 1:
+            disabled = "true"
+        else:
+            disabled = "false"
+        print('<script>document.getElementById("previous").disabled   = ' + disabled + ';</script>')
         print('<script>document.getElementById("reload").disabled     = false;</script>')
-        print('<script>document.getElementById("next").disabled       = episode.index >= (length - 1);</script>')
+        if self.__index >= len(self.__series["episodes"]) - 1:
+            disabled = "true"
+        else:
+            disabled = "false"
+        print('<script>document.getElementById("next").disabled       = ' + disabled + ';</script>')
         print('<script>document.getElementById("last").disabled       = false;</script>')
         
+        #print("index: " + str(self.__index))
         episode = self.__series["episodes"][self.__index]
         data    = "Episode #" + episode["number"]
         data   += "<h1>" + episode["story"] + " - Part" + episode["part"] + "</h1>"
         data   += "<img src=\"../" + self.__title + "/episodes/" + self.__title + "_" + episode["number"] + ".jpg\">"
         data   += "<p>" + episode["notes"] + "</p>"
-        print(data)
+        #self.__setDiv("view", data)
         
         length              = len(self.__series["episodes"])
         if  (self.__action == "about"):
-            self.__setDiv("view", self.__series["meta"]["about"])
+            data            = self.__series["meta"]["about"]
         elif self.__action == "first":
             self.__index    = 0
-            self.__setDiv("view", data)
         # Add previous.
-        elif (self.__action == "about") or (self.__action == "reload"):
-            self.__setDiv("view", data)
         # Add next.
         elif self.__action == "last":
             self.__index    = len(self.__series["episodes"]) - 1
-            self.__setDiv("view", data)
         else:
-            raise ValueError("Invalid request.")
-        pass
+            #raise ValueError("Invalid request.")
+            pass
+        print(data)
+        #self.__setDiv("view", data)
     
-    ##  update
+    ##  setDiv
     #   @brief  updates page
     def __setDiv(self, div, data):
         #print("here")
